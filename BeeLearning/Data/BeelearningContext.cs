a5cde73ac20c57aaf04c1cdfcf670a8b0683aa4f
Danielly <daniellyfrenanda24@gmail.com>
2024-09-06 12:21:45 -0300

    commit inicial do projeto com login

---
+++ BeeLearning/Data/BeelearningContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Beelearning.Models;
+
+namespace Beelearning.Data
+{
+    public class BeelearningContext : DbContext
+    {
+        public BeelearningContext(DbContextOptions<BeelearningContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aula> Aulas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aula>().ToTable("Aula");
+        }
+    }
+}
---
+++ BeeLearning/Models/Aula.cs
@@ -0,0 +1,8 @@
+﻿namespace Beelearning.Models
+{
+    public class Aula
+    {
+        public Guid AulaId { get; set; }
+        public string Nome { get; set; }
+    }
+}
